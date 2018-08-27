44e7eaab050eb2f7f8e9c81071de1ceebe51883b
majki <michofilip@gmail.com>
2018-08-27 23:08:35 +0200

    vector algebra

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,17 +1,19 @@
-import complex.Complex
 import complex.Complex._
+import complex.ComplexVector
 
 object Main extends App {
-  for (y <- -1 to 1; x <- -1 to 1) {
-    val c = Complex(x, y)
-    println(x + y * i)
-  }
-
-//  val c = 1 + 1 * i
-//  println(expI(Math.PI / 2))
-
-  //  println(Complex(3, 4))
-  //  println(Complex(3, -4))
-  //  println(Complex(-3, 4))
-  //  println(Complex(-3, -4))
+    for (y <- -1 to 1; x <- -1 to 1) {
+        println(x + y * i)
+    }
+    
+    println("*******")
+    
+    val v1 = ComplexVector(1, 2 - i, -5 * i, -3)
+    println(v1)
+    println(-v1)
+    
+    val c1 = 1 + 2 * i
+    val c2 = 1 + 2 * i
+    println(c1 == c2)
+    
 }
--- src/main/scala/complex/ComplexVector.scala
+++ src/main/scala/complex/ComplexVector.scala
@@ -1,9 +1,44 @@
 package complex
 
-class ComplexVector() {
-
+class ComplexVector private(val dimension: Int, _coefficients: Map[Int, Complex]) {
+    private val coefficients: Map[Int, Complex] = _coefficients.filterNot {
+        case (_, c) => c == Complex(0)
+    }
+    
+    def get(index: Int): Option[Complex] = coefficients.get(index)
+    
+    def apply(index: Int): Complex = {
+        require(1 <= index && index <= dimension)
+        coefficients.getOrElse(index, Complex(0))
+    }
+    
+    def unary_+ : ComplexVector = new ComplexVector(dimension, coefficients)
+    
+    def unary_- : ComplexVector = new ComplexVector(dimension, coefficients.mapValues(c => -c))
+    
+    def +(that: ComplexVector): ComplexVector = {
+        require(this.dimension == that.dimension)
+        ComplexVector((for (i <- 1 to dimension) yield apply(i) + that.apply(i)): _*)
+    }
+    
+    def -(that: ComplexVector): ComplexVector = this + -that
+    
+    def *(complex: Complex): ComplexVector = new ComplexVector(dimension, coefficients.mapValues(c => c * complex))
+    
+    def dot(that: ComplexVector): ComplexVector = {
+        require(this.dimension == that.dimension)
+        ComplexVector((for (i <- 1 to dimension) yield apply(i) * that.apply(i)): _*)
+    }
+    
+    override def toString: String = (for (i <- 1 to dimension) yield apply(i)).mkString("(", ", ", ")")
 }
 
-object ComplexVector{
-
+object ComplexVector {
+    
+    def apply(complexSeq: Complex*): ComplexVector = {
+        val dimension = complexSeq.length
+        val coefficients = (1 to dimension).zip(complexSeq).toMap
+        new ComplexVector(dimension, coefficients)
+    }
+    
 }
