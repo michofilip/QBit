25d8834d4e77d9cab38ab108b7cf7185ddc91d21
majki <michofilip@gmail.com>
2018-08-30 07:43:31 +0200

    qs v0.0.1

--- src/main/scala/complex/QuantumState.scala
+++ src/main/scala/complex/QuantumState.scala
@@ -1,5 +1,22 @@
-//package complex
+package complex
+
+class QuantumState(complexVector: ComplexVector) {
+    val qbits: Int = {
+        val log = Math.log(complexVector.dimension) / Math.log(2)
+        val logInt = log.toInt
+        require(logInt == log)
+        logInt
+    }
+    
+    
+//    override def toString: String = {
+//        val coefficients: Seq[Complex] = for (i <- 1 to complexVector.dimension) yield complexVector(i)
 //
-//class QuantumState(val coefficients: Vector[Complex]) {
-//    val qbits: Int = (Math.log(coefficients.length) / Math.log(2)).toInt
-//}
+//        coefficients.filterNot(c => c == Complex(0)).mkString("(", "", ")")
+//    }
+}
+
+object QuantumState {
+    def apply(coefficients: Complex*): QuantumState = new QuantumState(ComplexVector(coefficients: _*))
+    
+}
